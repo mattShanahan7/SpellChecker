d6ec9b19259eaf7177fab5047367f91bd8db397a
Matthew <Matthew@matts-macbook-pro-6.local>
2016-12-02 13:50:52 -0500

    Initial Commit

---
+++ SpellChecker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SpellChecker
+//
+//  Created by Matthew on 12/2/16.
+//  Copyright © 2016 Matthew. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
